9bf35b636cfb5c10e38c94c97a76b34626867050
Seraj Muneer Faridy <moonxwaterx@gmail.com>
2026-04-04 21:35:31 +0530

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        if nums == []:
+            return False
+            
+        arr = {}
+        for num in nums:
+            if num not in arr:
+                arr.update({num: 1})
+                continue
+            
+            arr[num] += 1
+        
+        if max(arr.values()) > 1: return True
+        
+        else: return False
+            
