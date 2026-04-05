9df4335a36c67843cc68df9c2982d271b156588c
Seraj Muneer Faridy <moonxwaterx@gmail.com>
2026-04-05 10:41:23 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        arr = {}
+
+        for i in range(len(nums)):
+            arr[target - nums[i]] = i
+
+            '''
+            dict {index: rem value}
+
+            iterate over nums 
+            '''
+
+        for j in range(len(nums)):
+            if nums[j] in arr.keys() and arr.get(nums[j]) != j:
+                return [j, arr.get(nums[j])]
+            
+        return []
