34afa203ff00600345c9959aa7ee6d0340577421
Seraj Muneer Faridy <moonxwaterx@gmail.com>
2026-04-05 09:59:25 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        freq1 = {}
+        freq2 = {}
+
+        for char in s:
+            if char in freq1: freq1[char] += 1
+
+            else: freq1[char] = 0        
+        
+        for char in t:
+            if char in freq2: freq2[char] += 1
+
+            else: freq2[char] = 0
+
+        return freq1 == freq2
