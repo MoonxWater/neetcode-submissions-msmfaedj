59512697eab50658af61b65dd53af2b2a3a78428
Seraj Muneer Faridy <moonxwaterx@gmail.com>
2026-04-13 13:53:14 +0530

    Add: merge-strings-alternately - submission-4

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        '''
+        mistake: res += word[i] takes O(n) time as it creates a brand 
+        new string and copies all the elements thus making my prev
+        sol O(n^2) as compared to the optimal O(n).
+
+        to solve this problem, instead of res as a string, make it a list
+        and append the char at the end of the list which would take 
+        O(1)
+
+        before return use ''.join(res) to do this in O(n).
+        '''
+
+        i = 0
+        res = []
+
+        while i < len(word1) and i < len(word2):
+            res.append(word1[i])
+            res.append(word2[i])
+
+            i += 1
+            
+            
+        res.extend(word1[i:])
+        res.extend(word2[i:])
+
+        return ''.join(res)
