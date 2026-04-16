ef760bb78a91c1e545b2ea319992c923f7b8f80c
Seraj Muneer Faridy <moonxwaterx@gmail.com>
2026-04-16 13:37:10 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.py
@@ -0,0 +1,37 @@
+'''
+a prefix array initialised to the first element of strs array
+
+start with the second element and compare the prefix till a mismatch
+is found or len of prefix or current element is exhausted.
+
+if the len of prefix is exhausted, move on
+else if the len of current element if exhausted, the current element
+becomes prefix
+
+
+
+return prefix
+'''
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0]
+
+        for i in range(len(strs)):
+            if strs[i] == '': return ''
+            
+            j = 0
+
+            while j < len(strs[i]) and j < len(prefix):
+                if strs[i][j] != prefix[j]:
+                    prefix = prefix[:j]
+                    break
+                j += 1
+
+            else:
+                prefix = prefix[:min(len(strs[i]), len(prefix))]
+
+        
+        return prefix
+
+
