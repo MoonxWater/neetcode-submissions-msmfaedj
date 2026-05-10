8341bbe191b7d4f1b054d53b182e3c42aab22ece
Seraj Muneer Faridy <moonxwaterx@gmail.com>
2026-05-10 18:40:54 +0530

    Add: set-zeroes-in-matrix - submission-1

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-1.py
@@ -0,0 +1,30 @@
+'''
+keep a weight array equal to column of the matrix
+1. run a loop over each row and check if the element is zero
+2. if the current element is zero, make the corresponding
+    weight zero
+3. after inner loop, make the entire row zero
+
+4.run another loop and multiply each element with the 
+    corresponding weight
+Note: In place, return None
+'''
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        weights = [1] * len(matrix[0])
+
+        for i in range(len(matrix)):
+            found = False
+
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    weights[j] = 0
+                    found = True
+            if found:
+                matrix[i] = [0] * len(matrix[i])
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] *= weights[j]
+        
