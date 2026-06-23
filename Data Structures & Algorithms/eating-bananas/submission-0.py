f88c4519e701850aacec43b469f03fc5b71632ad
Seraj Muneer Faridy <moonxwaterx@gmail.com>
2026-06-23 14:57:36 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,38 @@
+'''
+make a cur_h to sum eating time
+res will store the best answer
+
+get max from the piles list
+set low to 1 and high to max
+while low <= high, run a loop
+calc mid of low and high, floor div
+run through the array and div each pile with mid and ceil it.
+add the result to cur_h
+if at anypoint cur_h exceeds h, break from inner loop
+if break, koko too slow, increase speed: low to mid + 1
+if did not break, koko either too
+fast or perfect: high to mid - 1, res = mid
+return res + 1
+'''
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low, high = 1, max(piles)
+        res = 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            cur_h = 0
+
+            for pile in piles:
+                cur_h += math.ceil(pile / mid)
+
+                if cur_h > h:
+                    break
+            else:
+                res = mid
+                high = mid - 1
+                continue
+            
+            low = mid + 1
+
+        return res
